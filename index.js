25fbff96adf790d01da70b2c89dbafc42fbe6303
klimdanick <klimdanick@gmail.com>
2024-11-18 22:00:43 +0100

    fix

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const server = https.createServer(options, (req, res) => {
 
     // Forward the request to the appropriate target
     const splitIndex = req.url.slice(1).indexOf("/")
-    req.url = req.url.slice(splitIndex)
+    if (splitIndex >= 0) req.url = req.url.slice(splitIndex)
     console.log(`target url: ${proxyTarget}${req.url}`);
 
     proxy.web(req, res, { target: proxyTarget }, (err) => {
