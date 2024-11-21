e8afa7c74d4213d01ea6eae27006ee63d2b4898f
klimdanick <klimdanick@gmail.com>
2024-11-21 19:29:58 +0100

    ip logging

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const defaultTarget = "http://vps.klimdanick.nl:8085"
 // Create the reverse proxy server
 const server = https.createServer(options, (req, res) => {
     // Match routes to target servers
-    console.log(`request url: ${req.url}`);
+    console.log(`request url: ${req.url}, ${req.socket.remoteAddress}`);
     const target = Object.keys(targetMap).find((prefix) =>
         req.url.startsWith(prefix)
     );
